d711b9730fe2595dd15d669c9b36b95a03328a7f
brownhead <jsull003@ucr.edu>
2013-06-09 16:36:59 -0700

    Added database boilerplate.

--- mnet/application.py
+++ mnet/application.py
@@ -2,3 +2,12 @@
 
 from flask import Flask
 app = Flask("mnet")
+
+import MySQLdb
+db_connection = MySQLdb.connect(
+	host = "cs166db.codecolorizer.com",
+	user = "cs166user",
+	passwd = "cs166pw",
+	db = "cs166db"
+)
+db = db_connection.cursor()
