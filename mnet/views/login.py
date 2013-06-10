cf50cb23228ad9c02bffd53b81fc15e8aa41cf4c
brownhead <jsull003@ucr.edu>
2013-06-09 18:42:12 -0700

    Added logout view.

--- mnet/views/login.py
+++ mnet/views/login.py
@@ -1,6 +1,6 @@
 import flask
 from mnet.application import app, db
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user, current_user
 import mnet.auth as auth
 
 @app.route("/login", methods = ["GET", "POST"])
@@ -22,3 +22,27 @@ def login():
             app.logger.info("User %s logged in succesfully.", username)
 
     return flask.render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    user_name = getattr(current_user, "user_id", "Unknown")
+
+    if current_user.is_authenticated():
+        # logout_user() cannot fail...
+        app.logger.info("%s succesfully logged out.", user_name)
+
+        logout_user()
+
+        flask.flash(
+            "You have successfully logged out. You may now log in as another "
+            "user or close this site.", category = "message"
+        )
+    else:
+        app.logger.info("User tried to log out when not logged in.")
+
+        flask.flash(
+            "You were not logged in and you are still not logged in.",
+            category = "error"
+        )
+
+    return flask.redirect(flask.url_for("login"))
