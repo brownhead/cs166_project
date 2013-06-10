b22b266b7d79014a76a46a856a69b37273a87361
Daniel de Haas <ddeha001@ucr.edu>
2013-06-09 18:25:21 -0700

    finished register functionality

--- mnet/views/register.py
+++ mnet/views/register.py
@@ -17,22 +17,39 @@ def register():
                                 return flask.render_template("register.html", user_fields = user_fields)
 
                 # register the new user
-                db.execute('INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', \
-                           (flask.request.form[user_fields[3].lower()], \
-                            flask.request.form[user_fields[4].lower()], \
-                            flask.request.form[user_fields[0].lower()], \
-                            flask.request.form[user_fields[1].lower()], \
-                            flask.request.form[user_fields[2].lower()], \
-                            flask.request.form[user_fields[5].lower()], \
-                            flask.request.form[user_fields[6].lower()], \
-                            flask.request.form[user_fields[7].lower()], \
-                            flask.request.form[user_fields[8].lower()], \
-                            flask.request.form[user_fields[9].lower()], \
-                            flask.request.form[user_fields[10].lower()]))
+                try:
+                        db.execute('INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', \
+                                   (flask.request.form[user_fields[3].lower()], \
+                                    flask.request.form[user_fields[4].lower()], \
+                                    flask.request.form[user_fields[0].lower()], \
+                                    flask.request.form[user_fields[1].lower()], \
+                                    flask.request.form[user_fields[2].lower()], \
+                                    flask.request.form[user_fields[5].lower()], \
+                                    flask.request.form[user_fields[6].lower()], \
+                                    flask.request.form[user_fields[7].lower()], \
+                                    flask.request.form[user_fields[8].lower()], \
+                                    flask.request.form[user_fields[9].lower()], \
+                                    flask.request.form[user_fields[10].lower()], \
+                                    '0'))
+                except Exception as e:
+                        flask.flash('UID taken')
+                        return flask.render_template("register.html", user_fields = user_fields)
 
                 # register new users likes
-                liked_genres = [g.strip() for g in flask.request.form[user_fields[10].lower()].split(',')]
-                print liked_genres
+                liked_genres = [g.strip() for g in flask.request.form[user_fields[11].lower()].split(',')]
+
+                # generate list of genre names and ids
+                genre_ids = {}
+                if len(liked_genres) > 0:
+                        db.execute('SELECT * FROM genre')
+                        genres = db.fetchall()
+                        for genre in genres:
+                                genre_ids[genre[1].lower()] = genre[0]
+
+                for lg in liked_genres:
+                        if lg not in genre_ids:
+                                continue
+                        db.execute('INSERT INTO prefers VALUES (%s, %s)', (flask.request.form[user_fields[3].lower()], genre_ids[lg]))
 
                 return str('Sucessfully registered')
 
