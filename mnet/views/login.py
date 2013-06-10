d24df1b40a457980fb9b042cb566cf00a6ccb999
brownhead <jsull003@ucr.edu>
2013-06-09 18:36:55 -0700

    Finished login system.

--- mnet/views/login.py
+++ mnet/views/login.py
@@ -1,10 +1,24 @@
 import flask
 from mnet.application import app, db
+from flask.ext.login import login_user
+import mnet.auth as auth
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
-    if flask.request.method == 'POST':
-        username = flask.request.form['username']
-        password = flask.request.form['password']
-    else:
-        return flask.render_template('login.html')
+    if flask.request.method == "POST":
+        username = flask.request.form["username"]
+        password = flask.request.form["password"]
+
+        user = auth.User.fetch(username)
+        if user is None or user.password != password:
+            flask.flash("Bad user/password combination.", "error")
+
+            app.logger.info("User failed to login as %s.", username)
+        else:
+            login_user(user)
+
+            flask.flash("You have succesfully logged in.")
+
+            app.logger.info("User %s logged in succesfully.", username)
+
+    return flask.render_template("login.html")
